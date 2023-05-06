12a0a6a9862df40329cdf830580592ba84bbac46
HGCM <1228688831@qq.com>
2023-05-06 20:31:59 +0800

    fisrt commit

---
+++ springboot_01_04_officialstart/src/main/java/com/hgc/Springboot0104OfficialstartApplication.java
@@ -0,0 +1,13 @@
+package com.hgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0104OfficialstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot0104OfficialstartApplication.class, args);
+    }
+
+}
